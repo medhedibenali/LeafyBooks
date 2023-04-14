5061c73392001357b8969bf00324db4b9611602f
medhedibenali <moudou.madmoudou@gmail.com>
2023-04-14 22:26:39 +0100

    fix: updated path  to index.php

--- public/php/SignUpManager.php
+++ public/php/SignUpManager.php
@@ -4,4 +4,4 @@ require_once dirname(__FILE__, 3) . '/modules/auth/UserRepository.php';
 $userRepository = new UserRepository();
 $userRepository->insert($_POST);
 
-header('Location:  ../../index.php');
+header('Location:  ../index.php');
