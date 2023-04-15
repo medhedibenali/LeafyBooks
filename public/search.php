458ba471a79190c8b45332e78cd0f660f161eec6
zeineb <zeineb.benabdallah@insat.ucar.tn>
2023-04-15 23:48:10 +0100

    split the search into template and managaer

--- public/search.php
+++ public/search.php
@@ -2,27 +2,7 @@
 
 $pageTitle = 'Search';
 require_once dirname(__FILE__, 2) . '/templates/header.php';
-
-include_once dirname(__FILE__, 2) . '/modules/search/BooksRepository.php';
-$bookRepo = new BookRepository();
-if (!isset($_GET['search'])) {
-    return;
-}
-
-if (!isset($_GET['page'])) {
-    $page = 1;
-} else {
-    $page = $_GET['page'];
-}
-$results_per_page = 1;
-$page_first_result = ($page - 1) * $results_per_page;
-
-$search = htmlspecialchars($_GET['search']);
-$books = $bookRepo->findByTitleOrAuthor($search);
-$number_of_result = sizeof($books);
-$number_of_page = ceil($number_of_result / $results_per_page);
-$books = $bookRepo->findByTitleOrAuthorLimit($search, $page_first_result, $results_per_page)
-?>
+require_once 'php/SearchManager.php';?>
 <ul class="list-group">
     <?php
     foreach ($books as $book) {
@@ -30,16 +10,8 @@ $books = $bookRepo->findByTitleOrAuthorLimit($search, $page_first_result, $resul
         $title = $book->title;
         $synopsis = $book->synopsis;
         $author = $book->author;
-    ?>
-        <li class="book list-group-item">
-            <img src=<?= $cover ?> class="book_cover" />
-            <div class="book_info">
-                <a class="book_title"><?= $title ?></a>
-                <div class="book_author"> <span>author(s):</span> <?= $author ?></div>
-                <div class="book_synopsis"> <span>synopsis:</span> <?= $synopsis ?></div>
-            </div>
-        </li>
-    <?php } ?>
+        require dirname(__FILE__,2).'/templates/bookItem.php';
+     } ?>
 </ul>
 <div>
     <?php
---
+++ templates/bookItem.php
@@ -0,0 +1,8 @@
+<li class="book list-group-item">
+    <img src=<?= $cover ?> class="book_cover" />
+    <div class="book_info">
+        <a class="book_title"><?= $title ?></a>
+        <div class="book_author"> <span>author(s):</span> <?= $author ?></div>
+        <div class="book_synopsis"> <span>synopsis:</span> <?= $synopsis ?></div>
+    </div>
+</li>
