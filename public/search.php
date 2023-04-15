c524efa12f2e91a88ae22caa701e23db778bb307
zeineb <zeineb.benabdallah@insat.ucar.tn>
2023-04-16 00:30:18 +0100

    might add a menu for search

--- public/search.php
+++ public/search.php
@@ -3,16 +3,24 @@
 $pageTitle = 'Search';
 require_once dirname(__FILE__, 2) . '/templates/header.php';
 require_once 'php/SearchManager.php';?>
-<ul class="list-group">
-    <?php
-    foreach ($books as $book) {
+<div>
+    <form action="search.php" method="get">
+        <button  class="list-group-item" value="all" name="type">Search All books</button>
+        <button class="list-group-item" value="title" name="type">Search by book title</button>
+        <button class="list-group-item" value="author" name="type">All books</button>
+        <button class="list-group-item" value="user" name="type">All Users</button>
+    </form>
+    <ul class="list-group">
+        <?php
+        foreach ($books as $book) {
         $cover = $book->cover;
         $title = $book->title;
         $synopsis = $book->synopsis;
         $author = $book->author;
         require dirname(__FILE__,2).'/templates/bookItem.php';
-     } ?>
-</ul>
+        } ?>
+    </ul>
+</div>
 <div>
     <?php
     for ($page = 1; $page <= $number_of_page; $page++) { ?>
--- templates/userItem.php
+++ templates/userItem.php
@@ -1,1 +1,7 @@
-<?php
+<li class="book list-group-item">
+    <img src=<?= $profilePicture ?> class="book_cover" />
+    <div class="book_info">
+        <a class="book_title"><?= $username ?></a>
+        <div class="book_author"> <span>author(s):</span> <?= $firstName.' '.$lastName ?></div>
+    </div>
+</li>
