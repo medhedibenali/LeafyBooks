e57f268c818d442d0e430634c1e5a971922847cb
= <=>
2023-04-16 18:50:36 +0100

    chore: documented the Repository class and methods; documented the config file;

--- config/config.php
+++ config/config.php
@@ -1,8 +1,7 @@
 <?php
 
 /**
- * This code is used to connect to the database.
- * $config is an array of database configuration.
+ * $config is an array of configurations.
  * $config['db'] is an array of database configurations.
  * $config['db']['db1'] is the database configuration for db1.
  * $config['db']['db1']['dbname'] is the database name.
@@ -23,17 +22,22 @@ $config = array(
 );
 
 /**
- * Creating constants for heavily used paths makes things a lot easier. 
- * ex. require_once(LIBRARY_PATH . "Paginator.php")
+ * Constant for the modules directory.
  */
 
 defined("MODULES_PATH")
     or define("MODULES_PATH", realpath(dirname(__FILE__, 2) . '/modules'));
 
+/**
+ * Constant for the templates directory.
+ */
+
 defined("TEMPLATES_PATH")
     or define("TEMPLATES_PATH", realpath(dirname(__FILE__, 2) . '/templates'));
+
 /**
  * Error reporting.
  */
+
 ini_set("error_reporting", "true");
 error_reporting(E_ALL | E_STRICT);
