506ceb632ea954dd0e003c5e3a7b3f4ad0f53e0a
chadda <chadha.essid@insat.ucar.tn>
2023-04-18 17:38:59 +0100

    new stars for rating,modified file names

---
+++ public/php/add-review-&-rating-process.php
@@ -0,0 +1,45 @@
+<?php
+//include_once  dirname(__FILE__, 3) .'/modules/isAuthentificated.php';
+/*temporary until @mohamedHedi pushes the login page*/
+$_SESSION['user']['username']='amal';
+include_once dirname(__FILE__, 3) . '/modules/book_identification/BookRepository.php';
+/*review & rating processing*/
+include_once dirname(__FILE__, 3) . '/modules/book_identification/BookRepository.php';
+include_once  dirname(__FILE__, 3) .'/modules/book_identification/UserReviewsRepository.php';
+
+
+
+$review = $_POST['review'];
+$ISBN=$_POST['ISBN'];
+$rating = $_POST['rate'];
+$username=$_SESSION['user']['username']; //currently connected user
+$BookRepo=new BookRepository();
+$BookISBN=($BookRepo->find(["ISBN"=>$ISBN]))->ISBN;
+$UserReviewsRepo=new UserReviewsRepository();
+
+
+if(!($UserReviewsRepo->find(["ISBN"=>$ISBN,"username"=>$username])))
+{
+    $UserReviewsRepo->insert(["ISBN"=>$ISBN,"username"=>$username,"review"=>$review,"rating"=>$rating]);
+
+}
+else
+{
+
+}
+header("Location: " . $_SERVER['HTTP_REFERER']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tmp/template-rating-&-reviewing.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../public/css/book_identity.css">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Half Star Rating</h1>
+<fieldset class="rate">
+    <input type="radio" id="rating10" name="rating" value="10" /><label for="rating10" title="5 stars"></label>
+    <input type="radio" id="rating9" name="rating" value="9" /><label class="half" for="rating9" title="4 1/2 stars"></label>
+    <input type="radio" id="rating8" name="rating" value="8" /><label for="rating8" title="4 stars"></label>
+    <input type="radio" id="rating7" name="rating" value="7" /><label class="half" for="rating7" title="3 1/2 stars"></label>
+    <input type="radio" id="rating6" name="rating" value="6" /><label for="rating6" title="3 stars"></label>
+    <input type="radio" id="rating5" name="rating" value="5" /><label class="half" for="rating5" title="2 1/2 stars"></label>
+    <input type="radio" id="rating4" name="rating" value="4" /><label for="rating4" title="2 stars"></label>
+    <input type="radio" id="rating3" name="rating" value="3" /><label class="half" for="rating3" title="1 1/2 stars"></label>
+    <input type="radio" id="rating2" name="rating" value="2" /><label for="rating2" title="1 star"></label>
+    <input type="radio" id="rating1" name="rating" value="1" /><label class="half" for="rating1" title="1/2 star"></label>
+
+</fieldset>
+
+    <br>
+    <br>
+    <br>
+
+
+
+</form>
+
+</body>
+</html>
+
