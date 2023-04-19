a645ccdbad0973264236cc638baa734f680266dc
salma <salmakchaw123@gmail.com>
2023-04-19 18:10:49 +0100

    button animation issue

--- templates/js/BookTemplate.js
+++ templates/js/BookTemplate.js
@@ -2,3 +2,12 @@ function scrollTobottom()
 {
     window.scrollTo(0,document.body.scrollHeight);
 }
+const button=document.querySelector("#addtolist");
+button.addEventListener("click",e =>{
+    e.preventDefault();//to prevent the default behavior from occuring
+    button.classList.add("animate");
+    setTimeout(()=>{
+        button.classList.remove("animate");
+    },600)//we chose the timeout time same as the time of the animation of bottomBublles & top ones
+
+})
