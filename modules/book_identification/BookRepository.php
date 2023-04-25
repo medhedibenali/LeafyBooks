b626e24d77c83ddceb8d577d0c5411a379092402
salma <salmakchaw123@gmail.com>
2023-04-26 00:47:12 +0100

    adding column page_number to table books --> changed things book-identity

--- modules/book_identification/BookRepository.php
+++ modules/book_identification/BookRepository.php
@@ -8,7 +8,7 @@ class BookRepository extends Repository
     public function __construct()
     {
         $tableName = 'books';
-        $attributes = ['ISBN', 'title', 'author', 'publisher', 'picture','publishing_year','rating','genre'];
+        $attributes = ['ISBN', 'title', 'author', 'publisher', 'picture','publishing_year','rating','genre','page_number'];
         $ids = ['ISBN'];
         parent::__construct($tableName, $attributes, $ids);
     }
