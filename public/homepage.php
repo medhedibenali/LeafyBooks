3498ead3357ed41bdd9571cdb05d43dae27ddc1d
chadda <chadha.essid@insat.ucar.tn>
2023-05-07 02:45:25 +0100

    added icons and stars

--- public/homepage.php
+++ public/homepage.php
@@ -9,7 +9,8 @@ $stylesheets = array(
     'css/header.css',
     'css/categories.css',
     'css/carousel-principal.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    "https://unpkg.com/bootstrap-icons@1.6.1/font/bootstrap-icons.css"
 );
 
 $pic1="img/royal.jpg";
@@ -26,9 +27,22 @@ require TEMPLATES_PATH . '/header.php';
 <div class="ad">
 <div class="hp-anniversary-text">
     <h1>
-         <span class="magic">
-             <span style="color:#73B100;">Join us in celebrating 25 years of magic</span>
-         </span>
+       Join us in celebration of 25 years
+        <span class="magic">
+            <?php
+        for ($i = 0; $i < 3; $i++)
+        {
+            ?>
+            <span class="magic-star">
+            <svg viewBox="0 0 512 512">
+              <path d="M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z" />
+            </svg>
+          </span>
+<?php
+}
+?>
+    <span class="magic-text">magic</span>
+  </span>
     </h1>
 </div>
 <?php
@@ -36,7 +50,7 @@ require TEMPLATES_PATH.'/book-carrousel.php';
 ?>
     <h1 class="carousel-footer">
         <span class="magic">
-            <span style="color:#73B100;">with the brand-new illustrated collection by Jim Kay</span>
+            <span style="color:plum; font-family: DecoType Naskh">with the brand-new illustrated collection by Jim Kay</span>
          </span>
     </h1>
     <i class="fa-regular fa-ufo"></i>
@@ -48,6 +62,7 @@ require TEMPLATES_PATH .'/categories.php';
 $scripts=array(
     "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js",
     "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js",
-    "https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    "https://code.jquery.com/jquery-3.2.1.slim.min.js",
+    "js/magic-text.js"
 );
 require TEMPLATES_PATH.'/footer.php';
--- templates/categories.php
+++ templates/categories.php
@@ -1,11 +1,51 @@
 <?php
 $icons=array(
-'fantasy'=>"fas fa-wand-sparkles",
-'sc-fi'=>"fa-solid fa-user-astronaut",
-'thriller'=>"fa-solid fa-user-secret",
-'romance'=>"fa-regular fa-heart",
-'young adult'=>"fas fa-transporter",
+'fantasy'=>"fas fa-wand-sparkles fa-2x",
+'sciFi'=>"fa-solid fa-user-astronaut fa-2x",
+'thriller'=>"fa-solid fa-user-secret fa-2x",
+'romance'=>"fa-regular fa-heart fa-2x",
+'youngAdult'=>"bi bi-cloud-rain-fill cloudicon",
 );
+
+$genrePicture=array(
+    'fantasy'=>"img/fantasy.png",
+    'sciFi'=>"img/sci-fi.png",
+    'thriller'=>"img/thriller.png",
+    'romance'=>"img/romance.png",
+    'youngAdult'=>"img/ya.png",
+);
+
+$fantasyString="Do you feel like going  on
+ fantastical adventures, heroic battles, and epic journeys to save people, kingdoms, 
+ or dynasties ? In a world of your own imagination, fantasy explores the magical and mythical unknown, 
+ ancient folklore, and mythical creatures. Are you looking for characters like  a soon-to-be queen living in a magical kingdom, 
+ or a dragon riding the winds of an ancient land? Books penning the next Descendants of the Crane, 
+The Witcher, or Lord of the Rings or those classics themselves are then for you.";
+
+$thrillerString="Do you like books that keep you  on the edge of your seat and glued to your screen. 
+From solving crimes to racing against the clock to stop one from happening, 
+the Mystery & Thriller genre is packed with stories having suspenseful plots, high stakes, 
+and satisfying endings. Stories in this category will help bring out your inner detective! 
+Spanning from bestsellers like Gone Girl, When No One Is Watching, or Behind Her Eyes to hidden-gems for you to discover ";
+
+$sciFiString="This genre celebrates the ahead-of-their-age books  who evoke a sense of wonder, 
+unravel new realities, and send us shooting into the void. 
+The Science Fiction genre recognizes stories that center around science and technology—like Star Trek, 
+Fahrenheit 451, or The Fifth Season—and challenge us to think big and differently about the world we live in. 
+Space operas set in a galaxy far away, near-future dystopias, alternate histories..you get the picture.";
+
+$romanceString="This award celebrates the swoon-worthy storytellers who tug on our heartstrings 
+and demand our emotional investment. From passionate cowboys to billionaire rendezvous and 
+squee-worthy soulmates, these stories focus on adult characters and themes within modern, real-world settings. 
+The Romance genre is all about stories that depict a central love story that compels us to hope for a happy ending. 
+This category includes stories like The Kiss Quotient, The Hating Game, or The Wedding Date.";
+
+$youngAdultString="This award champions stories that explore the teen experience, from coping with loss to exploring gender, 
+and navigating high-school crushes and first romantic experiences. The Young Adult genre recognizes modern, 
+real-world stories that capture an authentic teen voice and the emotional stakes of growing up. 
+Be prepared for coming-of-age stories from all walks of life, 
+similar to the likes of To All the Boys I’ve Loved Before, The Hate U Give, and Simon vs. the Homo Sapiens Agenda.";
+
 ?>
 <div class="category-box">
     <div class="discover-more">
@@ -21,12 +61,12 @@ $icons=array(
     <div class="flex-item" >
         <div class="picture">
             <div class="category-head">
-            <p class="genre"><?=$key?></p>
-                <i class="<?=$value?>"></i>
+                <p class="genre"><?=$key?></p>
+                <i class="<?=$value?>" style="color:white;" id="my-icons"></i>
             </div>
-            <img src="img/fantasy.png">
+            <img src="<?=$genrePicture[$key]?>" class="genre-picture">
             <p class="genre-description">
-                This award recognizes the worldbuilders; the writers who take us on fantastical adventures, heroic battles, and epic journeys to save people, kingdoms, or dynasties. In a world of your own imagination, fantasy explores the magical and mythical unknown, ancient folklore, and mythical creatures. Is your character a soon-to-be queen living in a magical kingdom, or a dragon riding the winds of an ancient land? Writers penning the next Descendants of the Crane, The Witcher, or Lord of the Rings entered their stories in this category.
+                <?=${$key . "String"}?>
             </p>
         </div>
         <div class="carousel-books">
