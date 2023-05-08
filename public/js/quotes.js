d472e5b65d95bce8ae9f63a3deb64c150a65facd
chadda <chadha.essid@insat.ucar.tn>
2023-05-08 21:12:10 +0100

    fixed minor issue

--- public/js/quotes.js
+++ public/js/quotes.js
@@ -2,7 +2,12 @@ const quotes = document.querySelectorAll('.quote-box .quote-author');
 let box=document.querySelector(".quote-box");
 let currentQuoteIndex = 0;
 
-function showNextQuote() {
+function showNextQuote()
+{
+    quotes[quotes.length-1].querySelector('.quote').style.display = 'none';
+    quotes[quotes.length-1].querySelector('.author').style.display = 'none';
+
+
     // Hide the previous quote and author
     if (currentQuoteIndex > 0) {
         const prevQuote = quotes[currentQuoteIndex - 1];
@@ -21,17 +26,18 @@ function showNextQuote() {
     // If we've shown all the quotes, start over from the beginning
     if (currentQuoteIndex >= quotes.length) {
         currentQuoteIndex = 0;
-
         // Hide all quotes and authors
-        for (let i = 0; i < quotes.length; i++) {
+        for (let i = 0; i < quotes.length-1; i++) {
             const quote = quotes[i];
             quote.querySelector('.quote').style.display = 'none';
             quote.querySelector('.author').style.display = 'none';
         }
+
     }
 
     // Schedule the next quote to be shown after a delay
     setTimeout(showNextQuote, 3000); // 3000ms = 3 seconds
+
 }
 
 // Hide all quotes and authors initially
