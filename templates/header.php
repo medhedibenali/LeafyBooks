b9969ebb8ec7f9da81c794c29003a5823bc9cfe3
Mohamed Hedi Ben Ali <mohamedhedi.benali02@gmail.com>
2023-05-12 07:26:03 +0100

    fix: rectify paths

--- templates/header.php
+++ templates/header.php
@@ -38,7 +38,7 @@ if (isset($_SESSION['username'])) {
 <!--  NAVBAR   -->
 <nav class="navbar navbar-expand-lg t d-flex justify-content-center sticky-lg-top">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><i class="fa-solid fa-leaf"></i> LeafyBooks</a>
+        <a class="navbar-brand" href="index.php"><i class="fa-solid fa-leaf"></i> LeafyBooks</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
